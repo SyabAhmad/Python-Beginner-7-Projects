b922123b36c7bcb245af6ec80daefaf7c536a532
SyabAhmad <syabblogger@gmail.com>
2023-04-15 11:51:51 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,74 @@
+# 1. Guess the Number Game
+# This is a simple Python game where the user has to guess a number between 1 and 100. The program will provide hints such as "too high" or "too low" until the user guesses the correct number.
+#
+# How to Play
+# Clone the repository
+# Navigate to the directory where the code is located
+# Run the command python guess_the_number.py to start the game
+# Follow the on-screen instructions to play the game
+# Requirements
+# Python 3.x
+# 8. Simple Chatbot
+# This is a simple Python program that uses natural language processing techniques to understand and respond to user input. The chatbot can answer basic questions and carry out simple tasks such as setting reminders or providing weather information.
+#
+# How to Use
+# Clone the repository
+# Navigate to the directory where the code is located
+# Run the command python chatbot.py to start the chatbot
+# Type in your message and press Enter to send it
+# The chatbot will respond with an appropriate message
+# Requirements
+# Python 3.x
+# Natural Language Toolkit (nltk) library
+# 11. Password Generator
+# This is a Python program that generates random passwords based on user specifications such as length and complexity.
+#
+# How to Use
+# Clone the repository
+# Navigate to the directory where the code is located
+# Run the command python password_generator.py to start the program
+# Follow the on-screen instructions to generate a password
+# Requirements
+# Python 3.x
+# 22. Fibonacci Sequence Generator
+# This is a Python program that generates the Fibonacci sequence up to a specified number of terms.
+#
+# How to Use
+# Clone the repository
+# Navigate to the directory where the code is located
+# Run the command python fibonacci_sequence.py to start the program
+# Follow the on-screen instructions to generate the Fibonacci sequence
+# Requirements
+# Python 3.x
+# 23. Binary to Decimal Converter
+# This is a Python program that converts binary numbers to decimal numbers.
+#
+# How to Use
+# Clone the repository
+# Navigate to the directory where the code is located
+# Run the command python binary_to_decimal.py to start the program
+# Follow the on-screen instructions to convert a binary number to decimal
+# Requirements
+# Python 3.x
+# 25. Command-Line Text Editor
+# This is a simple command-line text editor written in Python. The program allows users to create, edit, and save text files from the command line.
+#
+# How to Use
+# Clone the repository
+# Navigate to the directory where the code is located
+# Run the command python text_editor.py to start the program
+# Follow the on-screen instructions to create, edit, and save text files
+# Requirements
+# Python 3.x
+# 31. Random Quote Generator
+# This is a Python program that generates a random quote from a collection of quotes.
+#
+# How to Use
+# Clone the repository
+# Navigate to the directory where the code is located
+# Run the command python quote_generator.py to start the program
+# The program will display a random quote
+# Requirements
+# Python 3.x
+# Feel free to modify the code or add new features to the programs. If you have any questions or suggestions, feel free to contact me.
+
